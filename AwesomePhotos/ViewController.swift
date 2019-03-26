e51e50692e4dda0dfc091e3c67d78a90051f4761
Phạm Minh Quang <quangpham131097@gmail.com>
2019-03-26 08:45:56 +0700

    Initial Commit

---
+++ AwesomePhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomePhotos
+//
+//  Created by Apple on 3/26/19.
+//  Copyright © 2019 Minh Quang Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
